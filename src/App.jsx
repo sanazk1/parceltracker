474e7bcb85019787f01d57a915f3641664ad54de
Sana Zubair Khan <sanazk2008@gmail.com>
2020-11-07 15:11:28 +0100

    Organize the project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './assets/images/logo.svg';
+//import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Video site</h1>
+    </div>
+  );
+}
+
+export default App;
