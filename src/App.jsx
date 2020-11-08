80f0116dc5924598657586ddc9d75a4d31d0ec7b
Sana Zubair Khan <sanazk2008@gmail.com>
2020-11-08 18:03:26 +0100

    completed Header & header CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-//import logo from './assets/images/logo.svg';
-//import './css/App.css';
+//import logo from './assets/images/logo.png';
+import "./css/style.css";
+import Header from "./components/organisms/Header";
+import information from "./information.json"
 
 function App() {
+  console.log(information)
+  console.log("App component")
+  
   return (
     <div className="App">
-      <h1>Video site</h1>
+      <h1></h1>
+      <Header data={information[0]} />
     </div>
   );
 }
---
+++ src/components/organisms/Header.jsx
@@ -0,0 +1,17 @@
+//React core
+import React from "react";
+import logo from "../../assets/images/logo.png";
+ 
+export default function Header({data}) {
+    console.log("Header component")
+    console.log(data);
+
+    return (
+        <header className={"header"} >
+        <a href="#" >
+            <img src={logo} alt= "Logo of the yellow company" />
+        </a>
+       
+    </header>
+    );
+}
